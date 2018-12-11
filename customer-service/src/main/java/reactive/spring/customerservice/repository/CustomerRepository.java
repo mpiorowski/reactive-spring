1a9a9841e6b4018a3cdf7eb7c25714199b5a81b2
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-12-11 11:35:58 +0100

    gitignore

--- customer-service/src/main/java/reactive/spring/customerservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package reactive.spring.customerservice.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactive.spring.customerservice.model.Customer;
-
-public interface CustomerRepository extends ReactiveCrudRepository<Customer, String> {
-
-}
